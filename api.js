5796c454fe057b9810909f72d24ce9ed0e7fd858
jfjeffries <jon.f.jeffries@gmail.com>
2018-05-26 13:31:20 -0400

    finalmoreorless

--- api.js
+++ api.js
@@ -7,16 +7,20 @@ const p1NameSet = document.getElementById("fOneName");
 const p2NameSet = document.getElementById("fTwoName");
 const p1ImageSet = document.getElementById("fOneImage");
 const p2ImageSet = document.getElementById("fTwoImage");
-const p1StatWindow = document.getElementsByClassName("fOneStats");
-const p2StatWindow = document.getElementsByClassName("fTwoStats");
+const p1Str = document.getElementById("p1StrNum");
+const p1Def = document.getElementById("p1DefNum");
+const p2Str = document.getElementById("p2StrNum");
+const p2Def = document.getElementById("p2DefNum");
 const p1InitHp = document.getElementsByClassName("fOneHp");
 const p2InitHp = document.getElementsByClassName("fTwoHp");
+
 
 const selectP1 = document.querySelector('.p1Button');
 const selectP2 = document.querySelector('.p2Button');
 const fightButton = document.querySelector('.fightButton');
 
 selectP1.addEventListener('click', p1Search);
+selectP2.addEventListener('click', p2Search);
 // selectP2.addEventListener('click', p2Enter);
 // fightButton.addEventListener('click', battle);
 
@@ -24,16 +28,9 @@ function p1Search(a){
     let pokeName = p1Input.value;
     console.log(pokeName);
 
-    if(pokeName.trim() == ""){
-        alert("Enter a pokemon!");
-    // }else if (typeof pokeName == 'number'){
-    //     url = baseURL + pokeName;
-    //     console.log(pokeName);
-    //     fetch(url)
-    //     .then(response => {
-    //         return response.json()
-    //     }).then(data => {console.log(data)})
-    } else {
+        if(pokeName.trim() == "") {
+            alert("Enter a pokemon!");
+        } else {
         url = baseURL + pokeName.toLowerCase();
         // console.log(url);
         
@@ -47,11 +44,10 @@ function p1Search(a){
         .then(data => {
             enterP1(data);
         })
-         
+      
     }
     
 }
-
 function enterP1(data){
     while (p1NameSet.firstChild){
         p1NameSet.removeChild(p1NameSet.firstChild);
@@ -62,17 +58,50 @@ function enterP1(data){
     let capName=capFirstName(data.name);
     console.log(capName)
     p1NameSet.innerText=capName;
-    p1ImageSet.innerHTML=`<img class= "fOneImageClass" src=${data.sprites.front_default} />`
+    p1ImageSet.innerHTML=`<img id= "fOneSprite" src=${data.sprites.front_default} />`
+    p1Str.innerText=data.stats[2].base_stat;
+    p1Def.innerText=data.stats[1].base_stat;
+}
+
+function p2Search(a){
+    let pokeName2 = p2Input.value;
+    console.log(pokeName2);
+
+        if(pokeName2.trim() == "") {
+            alert("Enter a pokemon!");
+        } else {
+        url = baseURL + pokeName2.toLowerCase();
+        // console.log(url);
+        
+        fetch(url, {
+            // mode: 'no-cors',
+        })
+        .then(function (response){
+            console.log(response)
+            return response.json()
+        })
+        .then(data => {
+            enterP2(data);
+        })
+      
+    }
     
 }
-// function p1Enter(a){
-//     e.preventDefault();
-//     // url = baseURL + 
-// }
+function enterP2(data){
+    while (p2NameSet.firstChild){
+        p2NameSet.removeChild(p2NameSet.firstChild);
+    }
+    while (p2ImageSet.firstChild){
+        p2ImageSet.removeChild(p2ImageSet.firstChild);
+    }
+    let capName=capFirstName(data.name);
+    console.log(capName)
+    p2NameSet.innerText=capName;
+    p2ImageSet.innerHTML=`<img id= "fTwoSprite" src=${data.sprites.front_default} />`
+    p2Str.innerText=data.stats[2].base_stat;
+    p2Def.innerText=data.stats[1].base_stat;
+}
 
-// function p2Enter(a){
-
-// }
 
 // function battle(x){
     
