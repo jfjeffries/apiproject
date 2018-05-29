df5b0937386c49193a0221e85eeb3f0c731166ea
jfjeffries <jon.f.jeffries@gmail.com>
2018-05-29 15:09:58 -0400

    initial dev push

--- api.js
+++ api.js
@@ -11,11 +11,14 @@ const p1Str = document.getElementById("p1StrNum");
 const p1Def = document.getElementById("p1DefNum");
 const p2Str = document.getElementById("p2StrNum");
 const p2Def = document.getElementById("p2DefNum");
-const p1InitHp = document.getElementsByClassName("fOneHp");
-const p2InitHp = document.getElementsByClassName("fTwoHp");
+const p1InitHp = document.getElementById("p1Hp");
+const p2InitHp = document.getElementById("p2Hp");
+const p1HpBox = document.getElementById(fOneHpNum);
+const p2HpBox = document.getElementById(fTwoHpNum);
 let p1Name;
 let p2Name;
-
+// console.log(p1InitHp)
+// console.log()
 
 const selectP1 = document.querySelector('.p1Button');
 const selectP2 = document.querySelector('.p2Button');
@@ -23,11 +26,11 @@ const fightButton = document.querySelector('.fightButton');
 
 selectP1.addEventListener('click', p1Search);
 selectP2.addEventListener('click', p2Search);
-// fightButton.addEventListener('click', battle);
+fightButton.addEventListener('click', battle);
 
 function p1Search(a){
     let pokeName = p1Input.value;
-    console.log(pokeName);
+    // console.log(pokeName);
 
         if(pokeName.trim() == "") {
             alert("Enter a pokemon!");
@@ -39,7 +42,7 @@ function p1Search(a){
             // mode: 'no-cors',
         })
         .then(function (response){
-            console.log(response)
+            // console.log(response)
             return response.json()
         })
         .then(data => {
@@ -57,7 +60,7 @@ function enterP1(data){
         p1ImageSet.removeChild(p1ImageSet.firstChild);
     }
     let capName=capFirstName(data.name);
-    console.log(capName)
+    // console.log(capName)
     p1NameSet.innerText=capName;
     p1Name=capName;
     p1ImageSet.innerHTML=`<img id= "fOneSprite" src=${data.sprites.front_default} />`
@@ -67,7 +70,7 @@ function enterP1(data){
 
 function p2Search(a){
     let pokeName2 = p2Input.value;
-    console.log(pokeName2);
+    // console.log(pokeName2);
 
         if(pokeName2.trim() == "") {
             alert("Enter a pokemon!");
@@ -79,7 +82,7 @@ function p2Search(a){
             // mode: 'no-cors',
         })
         .then(function (response){
-            console.log(response)
+            // console.log(response)
             return response.json()
         })
         .then(data => {
@@ -97,46 +100,125 @@ function enterP2(data){
         p2ImageSet.removeChild(p2ImageSet.firstChild);
     }
     let capName=capFirstName(data.name);
-    console.log(capName)
     p2NameSet.innerText=capName;
     p2Name=capName;
     p2ImageSet.innerHTML=`<img id= "fTwoSprite" src=${data.sprites.front_default} />`
     p2Str.innerText=data.stats[2].base_stat;
     p2Def.innerText=data.stats[1].base_stat;
+    
 }
 
-
-// function battle(x){
-//     let p1HP=p1InitHp;
-//     let p2HP=p2InitHp;
-    
-//     let p2AttackStr=p2Str;
-//     let p1DefStr=p1Def;
-//     let p2DefStr=p2Def;
-
-//     function p1Attack() {
-//         let p1AttackStr=p1Str;
-//         let p2DefStr=p2Def;
-//         let hitPower= math.random();
-//         let defPower=math.random();
-
-//         p1AttackStr=p1AttackStr*hitPower;
-//         p2DefStr=p2DefStr*defPower;
-//         let hit=p1AttackStr-p2DefStr;
-//         if (hit>0) {
-//             for (hit; hit > 0; hit--) {
-//             console.log(hit);
-//             p2HP -= hit;
-//             p2NameSet.innerText=p2HP;
-//         } else {
-//             console.log("miss");
-//         }
-//       }
-//     }
+var haveWinner=false;
+function battle(){
+    let p1HP=document.getElementById("p1HP").innerText;
+    let p2HP=document.getElementById("p2HP").innerText;
     
     
+    let p1AttackStr=p1Str.innerText;
+    let p2AttackStr=p2Str.innerText;
+    let p1DefStr=p1Def.innerText;
+    let p2DefStr=p2Def.innerText;
+    
+    if (p1Name && p2Name){
+        turn(haveWinner);
+    } else {
+        alert ("Enter combatants!");
+    }
 
-// }
+    function turn(haveWinner){
+        // if(!haveWinner){
+        // setTimeout(p1Attack, 2000);
+        // setTimeout(p2Attack, 3000); 
+        // } else {
+        //     console.log('is this working?')
+        // }
+        let k=0
+        do {
+            setTimeout(p1Attack, 2000);
+            setTimeout(p2Attack, 3000);
+            console.log("turn: " + k);
+            k++
+    } while(!haveWinner);
+    }
+    function p1Attack() {
+        let hitPower= randomGen();
+        let defPower=randomGen();
+        p1AttackStr*=hitPower;
+        p2DefStr*=defPower;
+        let hit=p1AttackStr-p2DefStr;
+        
+        if (hit>0) {
+            
+            p2HP -= Math.round(hit);
+            if (p2HP <=0){
+                document.getElementById("p2HP").innerText="RIP";
+                haveWinner=true;
+                console.log(p1Name + " wins");                
+                return haveWinner;
+                //p1wins;
+                //refresh page?
+            } else {
+                document.getElementById("p2HP").innerText=p2HP;
+                return;
+            }
+            
+            //add graphic to show hit
+        } else {
+            console.log("P1 missed");
+        }
+      
+    }
+    function p2Attack() {
+        let p1AttackStr=p1Str;
+        let p2DefStr=p2Def;
+        let hitPower=Math.random();
+        let defPower=Math.random();
+
+        p2AttackStr=p2AttackStr*hitPower;
+        p1DefStr=p1DefStr*defPower;
+        let hit=p2AttackStr-p1DefStr;
+        if (hit>0) {
+            p1getHit();
+            p1HP -= Math.round(hit);
+            if (p1HP <=0){
+                document.getElementById("p1HP").innerText="RIP";
+                haveWinner=true;
+                console.log(p2Name + " wins");
+                return haveWinner;
+                
+                //p2wins;
+            } else {
+                document.getElementById("p1HP").innerText=p1HP;
+                return;
+            }
+            //add graphic to show hit
+        } else {
+            console.log("p2 missed");
+        }
+      
+    }
+function p1getHit(){
+    setTimeout(turnRed, 2500);
+    setTimeout(turnClear, 2500);
+    setTimeout(turnRed, 250);
+    setTimeout(turnClear, 250);
+    turnClear();
+    return
+    function turnRed(){
+        document.getElementById("fOneHpNum").style.backgroundColor="red";
+    }
+    function turnWhite(){
+        document.getElementById("fOneHpNum").style.backgroundColor="white";
+    }
+    function turnClear(){
+        document.getElementById("fOneHpNum").style.backgroundColor="transparent";
+    }
+
+    
+
+}
+
+}
 function capFirstName(x){
     for (let j in x){
         if (j == 0) {
@@ -149,7 +231,6 @@ function capFirstName(x){
     }
     return x;
 }
-function randomGen(z){
-    z= math.random(z);
-    return z;
+function randomGen(){
+    return Math.random();
 }
